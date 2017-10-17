d115650f49ed366bdffe278baf5b308f30687ddf
javafa <nigne2016@gmail.com>
2017-10-17 16:29:29 +0900

    Initial commit

---
+++ app/src/main/java/com/veryworks/android/bicycle/model/Json.java
@@ -0,0 +1,22 @@
+package com.veryworks.android.bicycle.model;
+
+public class Json
+{
+    private GeoInfoBikeConvenientFacilitiesWGS GeoInfoBikeConvenientFacilitiesWGS;
+
+    public GeoInfoBikeConvenientFacilitiesWGS getGeoInfoBikeConvenientFacilitiesWGS ()
+    {
+        return GeoInfoBikeConvenientFacilitiesWGS;
+    }
+
+    public void setGeoInfoBikeConvenientFacilitiesWGS (GeoInfoBikeConvenientFacilitiesWGS GeoInfoBikeConvenientFacilitiesWGS)
+    {
+        this.GeoInfoBikeConvenientFacilitiesWGS = GeoInfoBikeConvenientFacilitiesWGS;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [GeoInfoBikeConvenientFacilitiesWGS = "+GeoInfoBikeConvenientFacilitiesWGS+"]";
+    }
+}
